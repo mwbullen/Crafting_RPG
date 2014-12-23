e6b67ac8a3aee7f490026fde5c6dae496ce86dff
mwbullen <mwbullen@mikes-mac-mini.home>
2014-12-22 22:38:33 -0500

    Revamped FSMs on hero

---
+++ Assets/Navigation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Navigation : MonoBehaviour {
+	NavMeshAgent navAgent;
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void NavtoTarget(GameObject target) {
+		navAgent.SetDestination (target.transform.position);
+
+	}
+
+	public bool HasArrived() {
+		if (navAgent.remainingDistance <= navAgent.stoppingDistance) {
+			return true;
+				}
+		return false;
+	}
+}
