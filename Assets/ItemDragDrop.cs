dc60eb37599477206126e7e35bbe1dbc502d207b
mwbullen <mwbullen@mikes-mac-mini.home>
2015-01-20 17:02:57 -0500

    UI changes
    Made generic ui inventory display

---
+++ Assets/ItemDragDrop.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ItemDragDrop : MonoBehaviour {
+
+	public void onDrag() {
+		transform.position = Input.mousePosition;
+
+	}
+
+}
